fc7cc4e5906f301dd89f01c6576c112cee1ba743
kratos1609 <67956131+kratos1609@users.noreply.github.com>
2021-02-01 18:18:17 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,5 @@
+radius=int(input("Input radius of the circle\n"))
+
+area=3.14*(radius**2)
+
+print("The area of the circle with radius ",radius," is: ",area)
---
+++ extension.py
@@ -0,0 +1,11 @@
+file_name=input("Input the file name\n")
+
+ext=file_name[-2:]
+
+#print(ext)
+
+if(ext=="py"):
+     print("The extension of the file is:\'python\'")
+
+else:
+     print("Unknown extension")
